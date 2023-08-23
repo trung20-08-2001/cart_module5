43b51272b433de19f3957cb8dd8eb1ada4adb999
Trung <nguyenvantrung200819dtd@gmail.com>
2023-08-23 10:42:18 +0700

    fix route

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import ProductList from './components/ProductList'
 import { Route, Routes } from 'react-router'
 import NotFound from './components/NotFound'
 import Detail from './components/Detail'
-import Cart from "./components/Cart"
+import Cart from "./components/Cart";
+import { HashRouter } from "react-router-dom";
 
 const App = () => {
     return (
         <>
+            <HashRouter>
+                <Routes>
+                    <Route path='/' element={<Navbar />}>
+                        <Route path='/' index element={<ProductList />}></Route>
+                        <Route path='/detail/:id' element={<Detail />}></Route>
+                        <Route path='/cart' element={<Cart />} />
+                    </Route>
+                    <Route path='*' element={<NotFound />}></Route>
+                </Routes>
+            </HashRouter>
 
-            <Routes>
-                <Route path='/' element={<Navbar />}>
-                    <Route path='/home' index element={<ProductList />}></Route>
-                    <Route path='/detail/:id' element={<Detail />}></Route>
-                    <Route path='/cart' element={<Cart/>} />
-                </Route>
-                <Route path='*' element={<NotFound />}></Route>
-            </Routes>
         </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
